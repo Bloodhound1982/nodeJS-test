94ce7d4e6c8f015055f7d73c9859df0ce93018ee
Alex <bloodhound1982@gmail.com>
2017-02-20 19:41:34 +0200

    firs commit

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 'use strict';
 
-let express = require('express');
-let app = express();
+let express = require('express'),
+    app = express(),
+    path = require('path'),
+    bodyParser = require('body-parser');
+
+app.get('/', function (req, res) {
+    res.send('Hello API');
+});
+
 
 module.exports = app;
